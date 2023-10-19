d653647ec80e0c9085461db906b384bfbc6a5358
heydarov93 <heyderov.yashar@mail.ru>
2023-10-19 09:05:04 -0700

    Fizz-Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: on Success 0
+ *
+ */
+
+int main(void)
+{
+	int i = 3;
+
+	printf("1 2 "); 
+
+	while (i <= 100 )
+	{
+		if(i % 3 == 0 && i % 5 == 0)
+			printf(" FizzBuzz");
+		else
+		{
+			if(i % 3 == 0)
+				printf(" Fizz");
+			else
+			{	if(i % 5 == 0)
+					printf(" Buzz");
+				else
+					printf(" %d", i);
+			}
+		}
+		i++;
+	}
+	
+	printf("\n");
+	return (0);
+}
