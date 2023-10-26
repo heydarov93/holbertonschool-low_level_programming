afec716d15e7f802d07bfd18989c0054dcda6f2c
heydarov93 <heyderov.yashar@mail.ru>
2023-10-26 04:05:43 -0700

    strncat

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -4,8 +4,8 @@
 /**
  * _strncat - concatenates two strings
  *
- * @dest: char
- * @src: char
+ * @dest: pointer to char
+ * @src: pointer to char
  * @n: int
  *
  * Return: the pointer to dest
@@ -18,7 +18,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	lengthDEST = strlen(dest);
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		*(dest + (lengthDEST + i)) = *(src + i);
 
 	return (dest);
