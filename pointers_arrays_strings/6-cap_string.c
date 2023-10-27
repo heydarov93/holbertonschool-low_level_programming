5a9577730442ede2a82e454883f3d14ddd5805aa
heydarov93 <heyderov.yashar@mail.ru>
2023-10-27 08:54:48 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * uppercase
+ *
+ * @s: pointer to char
+ *
+ * Return: pointer to char
+ */
+
+char *cap_string(char *s)
+{
+	int i, j;
+	char separators[13] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		for (j = 0; j < 13; ++j)
+		{
+			if (s[i] == separators[j])
+			{
+				if (s[i + 1] >= 97 && s[i + 1] <= 122)
+				{
+					s[i + 1] = s[i + 1] - 32;
+					i++;
+					j = 13;
+				}
+			}
+		}
+	}
+
+	return (s);
+}
