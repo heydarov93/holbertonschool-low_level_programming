70a879d0cb9e8bef3919133c1a059ed755f591b9
heydarov93 <heyderov.yashar@mail.ru>
2023-10-27 08:59:55 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -14,6 +14,9 @@ char *cap_string(char *s)
 	int i, j;
 	char separators[13] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
+	if (s[0] >= 97 && s[0] <= 122)
+		s[0] = s[0] - 32;
+
 	for (i = 0; s[i] != '\0'; ++i)
 	{
 		for (j = 0; j < 13; ++j)
