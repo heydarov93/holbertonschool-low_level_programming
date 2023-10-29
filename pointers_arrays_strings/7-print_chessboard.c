4be7c25e0a78e24058774e3d6b79f0f03cae88a2
heydarov93 <heyderov.yashar@mail.ru>
2023-10-29 03:59:17 -0700

    Chess is mental torture

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ *
+ * @a: pointer to multidimensional array[][8]
+ *
+ *
+ * Return: nothing
+ *
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+	int length = sizeof(a) / sizeof(char);
+
+	for (i = 0; i < length; ++i)
+	{
+		if (a[i][0] != ' ')
+		{
+			for (j = 0; j < 8; ++j)
+				_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
