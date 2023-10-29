041f85db5f6a002bb24b01e03a3122cc5be5754f
heydarov93 <heyderov.yashar@mail.ru>
2023-10-29 04:01:27 -0700

    Chess is mental torture

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -17,11 +17,9 @@ void print_chessboard(char (*a)[8])
 
 	for (i = 0; i < length; ++i)
 	{
-		if (a[i][0] != ' ')
-		{
-			for (j = 0; j < 8; ++j)
-				_putchar(a[i][j]);
-		}
+		for (j = 0; j < 8; ++j)
+			_putchar(a[i][j]);
+
 		_putchar('\n');
 	}
 }
