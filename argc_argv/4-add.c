21bbbffe21f73889526fc96406c4f3e28668a629
heydarov93 <heyderov.yashar@mail.ru>
2023-11-02 02:17:23 -0700

    To infinity and beyond

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -12,7 +12,7 @@
 
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, j, sum = 0;
 
 	if (argc < 2)
 		printf("0\n");
@@ -20,10 +20,13 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (*argv[i] < 48 || *argv[i] > 57)
+			for (j = 0; argv[i][j] != '\0'; j++)
 			{
-				printf("Error\n");
-				return (1);
+				if (argv[i][j] < 48 || argv[i][j] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
 
 			sum += atoi(argv[i]);
