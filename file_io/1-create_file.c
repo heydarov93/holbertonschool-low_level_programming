4b73b977109a15ee3d94700837ffeed6c33d2ef9
heydarov93 <heyderov.yashar@mail.ru>
2023-12-16 11:40:15 -0800

    text_content == NULL case is handled

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -24,12 +24,10 @@ int create_file(const char *filename, char *text_content)
 	if (fd)
 	{
 		if (text_content != NULL)
-		{
 			sz = write(fd, text_content, strlen(text_content));
 
-			if (close(fd) != -1 && sz)
-				return (1);
-		}
+		if (close(fd) != -1 && sz)
+			return (1);
 	}
 
 	return (-1);
