22c7ae531d7fcde399540c96787520ada899c36d
Yashar <98576250+heydarov93@users.noreply.github.com>
2023-12-17 23:24:00 +0400

    delete hash table

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -3,11 +3,8 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete - delete table
- * @ht: pointer to struct of hash table
- *
- * Description: free and delete hash table
- * Return: na
+ * hash_table_delete - a function that deletes a hash table
+ * @ht: the hash table
  */
 void hash_table_delete(hash_table_t *ht)
 {
