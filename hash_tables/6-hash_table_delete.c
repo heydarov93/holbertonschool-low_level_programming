c7853474c932c341af7b896eefb5d6fc3d2de9e7
Yashar <98576250+heydarov93@users.noreply.github.com>
2023-12-17 23:31:46 +0400

    Update 6-hash_table_delete.c

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -14,10 +14,10 @@ void hash_table_delete(hash_table_t *ht)
 
 	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[i] != NULL)
+		if (ht->array[i])
 		{
 			node = ht->array[i];
-			while (node != NULL)
+			while (node)
 			{
 				tmp = node->next;
 				free(node->key);
