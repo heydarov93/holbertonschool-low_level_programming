cc8d2228cc26ffdea7660c6dba2f656ba0b60fa2
heydarov93 <heyderov.yashar@mail.ru>
2023-12-17 06:39:57 -0800

    >>> ht = {}

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates ash table
+ * @size: is the size of the array
+ *
+ * Return: pointer to the newly created hash table
+ *		NULL on failure
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+	hash_node_t **arr;
+
+	arr = malloc(sizeof(char *) * size);
+
+	if (arr == NULL)
+	{
+		free(arr);
+		return (NULL);
+	}
+
+	table = malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+
+	(*table).size = size;
+	(*table).array = arr;
+
+	return (table);
+}
