67e2db18234113b55f44eb8341fed65bf4ec73d4
heydarov93 <heyderov.yashar@mail.ru>
2023-12-17 09:54:57 -0800

    checking if ht->array[index] not 0 case handled

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -19,7 +19,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (strlen(key) == 0 || ht == NULL)
 		return (0);
 
-	if (strcmp(ht->array[index]->key, key) == 0)
+	if (ht->array[index] && strcmp(ht->array[index]->key, key) == 0)
 	{
 		ht->array[index]->value = strdup(value);
 		return (1);
