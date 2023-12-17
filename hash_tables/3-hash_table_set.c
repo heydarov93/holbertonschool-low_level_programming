7bc134707c432e91f907758f128ab4f923cd3348
heydarov93 <heyderov.yashar@mail.ru>
2023-12-17 10:02:47 -0800

    assigning value to the index was moved below - after ht == NULL check

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -13,11 +13,13 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	unsigned long int index;
 	hash_node_t *node;
 
 	if (strlen(key) == 0 || ht == NULL)
 		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
 
 	if (ht->array[index] && strcmp(ht->array[index]->key, key) == 0)
 	{
